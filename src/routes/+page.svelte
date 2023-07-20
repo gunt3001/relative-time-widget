848087ad9427999b67a09070a597ba1a90269034
Napat R. on Hutao-Surface <napat.gunt3001@gmail.com>
2023-07-20 21:43:33 +0800

    Force prerender flag to be able to render static site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script lang="ts">
+	console.log('Hello from JS!');
+</script>
+
 <h1 class="text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
